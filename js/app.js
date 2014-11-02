2bd90bd5cab87ba71f8939a5d4d594f00ec2479a
cargosoon <cargosoon@gmail.com>
2014-11-02 11:44:22 +1000

    Working game. TODO - collision detection

--- js/app.js
+++ js/app.js
@@ -1,39 +1,90 @@
+var vert = 83;
+var horz = 100; 
+
+var Counter = function() { this.ii = 0; };
+Counter.prototype.inc = function(){this.ii++;};
+
+var enemySprites = ['images/enemy-bug-left.png', 'images/enemy-bug.png'];
+
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-}
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+	this.x = 0;
+	this.y = 90;
+	this.dir = 1; // for right, or -1 for left
+	this.speed = 250;
+    this.sprite = enemySprites[1];
+};
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
-
-// Draw the enemy on the screen, required method for game
+	if (this.x > 500 || this.x < -80) { 
+		this.dir = this.dir * -1;
+		this.speed = Math.floor(Math.random() * 10) * 20;
+	    if (this.dir == -1) {
+			this.sprite = enemySprites[0];
+		} else { 
+			this.sprite = enemySprites[1];
+		}
+	};
+	this.x = this.x + dt * this.dir * this.speed;
+};
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	var rr = Resources.get(this.sprite);
+    ctx.drawImage(rr, this.x, this.y);
+};
+Enemy.prototype.checkCollision = function (px, py) {
+	//noop
+};
+
+var Player = function() {
+	this.sprite = 'images/char-boy.png';
+	this.x = 202;
+	this.y = 405;
+};
+Player.prototype.update = function() { 
+/* I'm not sure what this was intended for exactly, but I use it here
+to check if player has stepped off the playing field */
+	if (this.y < 0) { 
+		this.x = 202; 
+		this.y = 405;
+	};
+	if (this.x < 0) { this.x = this.x + horz; };
+	if (this.x > 500) { this.x = this.x - horz; };
+	//console.log(this.x + ", " + this.y)
 }
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+Player.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(keyCode) {
+	if (keyCode === 'left') { 
+		this.x = this.x - horz; 
+		console.log("left"); 
+	};
+	if (keyCode === 'up') { 
+		this.y = this.y - vert; 
+		console.log("up"); 
+	};
+	if (keyCode === 'right') { 
+		this.x = this.x + horz; 
+		console.log("right"); 
+	};
+	if (keyCode === 'down') { 
+		this.y = this.y + vert; 
+		console.log("down"); 
+	};
+	//console.log("unknown key press " + keyCode);
+};
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [];
+for (var ii=0; ii<3; ii++) { allEnemies.push(new Enemy()); };
+allEnemies[0].y = 230;
+allEnemies[1].y = 145;
+allEnemies[1].dir = -1;
+allEnemies[1].x = 500;
+allEnemies[1].sprite = enemySprites[1];
+allEnemies[2].y = 65;
 
+var player = new Player();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
--- js/engine.js
+++ js/engine.js
@@ -1,45 +1,43 @@
 var Engine = (function(global) {
-    var doc = global.document,
-        win = global.window,
-        canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        patterns = {},
-        lastTime;
-
-    canvas.width = 505;
-    canvas.height = 606;
+    var doc = global.document;
+	var win = global.window;
+	var canvas = doc.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	var patterns = {};
+	var lastTime = Date.now();
+    
+	canvas.width = 505;
+    canvas.height = 616;
     doc.body.appendChild(canvas);
-
-    function main() {
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
-
+    
+	function main() {
+        var now = Date.now();
+		var dt = (now - lastTime) / 1000.0;
         update(dt);
         render();
-
         lastTime = now;
         win.requestAnimationFrame(main);
     };
-
     function init() {
-
         reset();
         lastTime = Date.now();
         main();
     }
-
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
-
     function updateEntities(dt) {
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
         player.update();
     }
-
+	function checkCollisions() {
+		allEnemies.forEach(function(enemy) {
+			enemy.checkCollision(player.x, player.y);
+		});
+	}
     function render() {
         var rowImages = [
                 'images/water-block.png',
@@ -78,6 +76,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
+		'images/enemy-bug-left.png',
         'images/char-boy.png'
     ]);
     Resources.onReady(init);
